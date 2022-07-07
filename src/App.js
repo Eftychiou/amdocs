637490804a0bb9b641aa02d1e77bc65504ba1a9d
George <eftichiou@hotmail.com>
2022-07-07 16:01:23 +0300

    amdocs

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import PanelList from "./components/PanelList";
+import PanelDetails from "./components/PanelDetails";
+import Backdrop from "./components/shared/Backdrop";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 function App() {
+  const [panelList, setPanelList] = useState(null);
+  const [selectedPanel, setSelectedPanel] = useState(null);
+
+  useEffect(() => {
+    axios
+      .get(`https://api.nextjs-shop.com/shop/test`)
+      .then(({ data }) => {
+        setPanelList(data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <PanelList
+        panelList={panelList}
+        selectedPanel={selectedPanel}
+        setSelectedPanel={setSelectedPanel}
+      />
+      <PanelDetails
+        panelList={panelList}
+        selectedPanel={selectedPanel}
+        setSelectedPanel={setSelectedPanel}
+      />
+      <Backdrop
+        selectedPanel={selectedPanel}
+        setSelectedPanel={setSelectedPanel}
+      />
+    </>
   );
 }
 
